79103b2ed78c00290b21b17a4be9a47c5e3c498e
kongzhen <2625882561@qq.com>
2020-11-06 02:47:04 +0800

    适配器

--- app/src/main/java/com/example/final_appwork/MyAdapter2.java
+++ app/src/main/java/com/example/final_appwork/MyAdapter2.java
@@ -7,6 +7,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
@@ -17,8 +21,15 @@ import java.util.List;
 public class MyAdapter2 extends ArrayAdapter {
     private static final String TAG = "MyAdapter";
 
+    List object;
+
     public MyAdapter2(Context context, int resource, List objects) {
         super(context, resource, objects);
+        object=objects;
+    }
+
+    public void notifyDataSetChanged() {
+        super.notifyDataSetChanged();
     }
 
     @Override
@@ -27,22 +38,27 @@ public class MyAdapter2 extends ArrayAdapter {
         ViewHolder holder;
         if (itemview == null) {
             itemview = LayoutInflater.from(getContext()).inflate(R.layout.listconfig2, parent, false);
-            /*holder = new ViewHolder();
-            holder.editText=convertView.findViewById(R.id.et_todo);
-            holder.radioButton=convertView.findViewById(R.id.radio_btn);
-            convertView.setTag(holder);*/
         }
+        //获取传来的TodoItem
+        final TodoItem map = (TodoItem) getItem(position);
 
-        //获取传来的memoItem
-        TodoItem map = (TodoItem) getItem(position);
-        TextView body = itemview.findViewById(R.id.list_todo_body);
+        final TextView body = itemview.findViewById(R.id.list_todo_body);
+       // RadioGroup rg=itemview.findViewById(R.id.radio_group);
+        final RadioButton rb=itemview.findViewById(R.id.radio_btn);
         //显示
         String state=map.getState();
         Log.i(TAG,"state"+state);
-        if(state.equals("0")){//未被选中，设置成黑色
-            body.setTextColor(Color.rgb(0,0,0));
-        }else{//被选中，设置为白色
-            body.setTextColor(Color.rgb(105,105,105));
+        if(state.equals("1")){//被选中，设置成灰色
+           // body.setTextColor(Color.rgb(0,0,0));
+           // rb.get
+            rb.setChecked(true);
+            rb.setBackgroundColor(Color.rgb(220,220,220));
+            body.setBackgroundColor(Color.rgb(220,220,220));
+        }else{//未选中，白色
+            //body.setTextColor(Color.rgb(105,105,105));
+            rb.setChecked(false);
+            rb.setBackgroundColor(Color.rgb(255,255,255));
+            body.setBackgroundColor(Color.rgb(255,255,255));
         }
         body.setText(map.getBody());
         return itemview;
